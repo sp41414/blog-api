e13994c290b077242218a48383c9519065f085ab
sp41414 <134581084+sp41414@users.noreply.github.com>
2025-12-01 07:03:24 +0400

    fix: cors origin

--- backend/app.js
+++ backend/app.js
@@ -4,18 +4,18 @@ const helmet = require("helmet");
 const cors = require("cors");
 const passport = require("./src/auth/passport");
 const routers = {
-    authRouter: require("./src/routes/authRoutes"),
-    postRouter: require("./src/routes/postRoutes"),
+  authRouter: require("./src/routes/authRoutes"),
+  postRouter: require("./src/routes/postRoutes"),
 };
 const limit = require("express-rate-limit");
 
 // brute force protection
 const limiter = limit({
-    windowMs: 1 * 60 * 1000, // 1 minute
-    limit: 100, // 100 requests every 1 minute
-    standardHeaders: "draft-8",
-    legacyHeaders: false,
-    ipv6Subnet: 56,
+  windowMs: 1 * 60 * 1000, // 1 minute
+  limit: 100, // 100 requests every 1 minute
+  standardHeaders: "draft-8",
+  legacyHeaders: false,
+  ipv6Subnet: 56,
 });
 
 const app = express();
@@ -23,27 +23,41 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
-    cors({
-        origin: process.env.FRONTEND_URL,
-        credentials: true,
-    }),
+  cors({
+    origin: (origin, cb) => {
+      // allows no origin requests
+      if (!origin) return cb(null, true);
+
+      const allowedDomain = process.env.FRONTEND_URL;
+      // dynamic domains like the one im hosting on (cloudflare pages)
+      if (
+        origin === process.env.FRONTEND_URL ||
+        origin.endsWith(allowedDomain)
+      ) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
 );
 app.use(limiter);
 app.use(passport.initialize());
 app.use("/auth", routers.authRouter);
 app.use("/posts", routers.postRouter);
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({
-        error: {
-            message: err.message,
-            timestamp: new Date().toISOString(),
-        },
-    });
+  console.error(err.stack);
+  res.status(500).json({
+    error: {
+      message: err.message,
+      timestamp: new Date().toISOString(),
+    },
+  });
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, (err) => {
-    if (err) throw err;
-    console.log(`App listening on PORT ${PORT}`);
+  if (err) throw err;
+  console.log(`App listening on PORT ${PORT}`);
 });
