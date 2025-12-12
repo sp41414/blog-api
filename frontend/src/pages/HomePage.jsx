72e096aabb94dc6f3f2439e6d1675d972ae9e22e
sp41414 <134581084+sp41414@users.noreply.github.com>
2025-12-13 03:47:17 +0400

    fix: post page loading before authentication check

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -7,84 +7,84 @@ import ErrorAlert from "../components/ui/ErrorAlert";
 import { AuthContext } from "../contexts/AuthContext";
 
 export default function HomePage() {
-  const { user } = useContext(AuthContext);
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState("");
+    const { user } = useContext(AuthContext);
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
 
-  useEffect(() => {
-    async function fetchPosts() {
-      try {
-        setLoading(true);
-        const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/posts`);
-        const posts = await res.json();
-        if (posts.posts && posts.posts.length !== 0) {
-          setPosts(posts.posts);
-          setError("");
-        } else {
-          setPosts([]);
-          setError(posts.message || "No posts yet");
+    useEffect(() => {
+        async function fetchPosts() {
+            try {
+                setLoading(true);
+                const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/posts`);
+                const posts = await res.json();
+                if (posts.posts && posts.posts.length !== 0) {
+                    setPosts(posts.posts);
+                    setError("");
+                } else {
+                    setPosts([]);
+                    setError(posts.message || "No posts yet");
+                }
+            } catch (err) {
+                console.error(err.message);
+                setError("Failed to load posts");
+                setPosts([]);
+            } finally {
+                setLoading(false);
+            }
         }
-      } catch (err) {
-        console.error(err.message);
-        setError("Failed to load posts");
-        setPosts([]);
-      } finally {
-        setLoading(false);
-      }
-    }
-    fetchPosts();
-  }, []);
+        fetchPosts();
+    }, []);
 
-  return (
-    <div className="flex flex-col min-h-screen bg-neutral-900 overflow-x-hidden">
-      <NavBar />
-      {loading && (
-        <main className="flex-1 flex flex-col items-center justify-center">
-          <Spinner size="xl" />
-        </main>
-      )}
-      {error && (
-        <main className="flex-1">
-          <ErrorAlert message={error} />
-        </main>
-      )}
-      {!loading && (
-        <>
-          <main className="flex-1 p-4">
-            <section className="flex pb-12 pt-12 items-center justify-center">
-              {user ? (
-                user.admin ? (
-                  <p className="text-4xl lg:text-5xl text-white">
-                    Welcome to your Blog,{" "}
-                    <span className="text-4xl lg:text-5xl text-emerald-500 hover:text-emerald-300 transition-colors duration-500">
-                      {user.username}
-                    </span>
-                  </p>
-                ) : (
-                  <p className="text-4xl lg:text-5xl text-white">
-                    Welcome to my Blog,{" "}
-                    <span className="text-4xl lg:text-5xl text-emerald-500 hover:text-emerald-300 transition-colors duration-500">
-                      {user.username}
-                    </span>
-                  </p>
-                )
-              ) : (
-                <p className="text-4xl lg:text-5xl text-white">
-                  Welcome to my&nbsp;
-                  <span className="text-4xl lg:text-5xl text-emerald-500 hover:text-emerald-300 transition-colors duration-500">
-                    Blog
-                  </span>
-                </p>
-              )}
-            </section>
-            <section className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8 max-w-[95%] ">
-              <PostsCards posts={posts} />
-            </section>
-          </main>
-        </>
-      )}
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="flex flex-col min-h-screen bg-neutral-900 overflow-x-hidden">
+            <NavBar />
+            {loading && (
+                <main className="flex-1 flex flex-col items-center justify-center">
+                    <Spinner size="xl" />
+                </main>
+            )}
+            {error && (
+                <main className="flex-1">
+                    <ErrorAlert message={error} />
+                </main>
+            )}
+            {!loading && (
+                <>
+                    <main className="flex-1 p-4">
+                        <section className="flex pb-12 pt-12 items-center justify-center">
+                            {user ? (
+                                user.admin ? (
+                                    <p className="text-4xl lg:text-5xl text-white">
+                                        Welcome to your Blog,{" "}
+                                        <span className="text-4xl lg:text-5xl text-emerald-500 hover:text-emerald-300 transition-colors duration-500">
+                                            {user.username}
+                                        </span>
+                                    </p>
+                                ) : (
+                                    <p className="text-4xl lg:text-5xl text-white">
+                                        Welcome to my Blog,{" "}
+                                        <span className="text-4xl lg:text-5xl text-emerald-500 hover:text-emerald-300 transition-colors duration-500">
+                                            {user.username}
+                                        </span>
+                                    </p>
+                                )
+                            ) : (
+                                <p className="text-4xl lg:text-5xl text-white">
+                                    Welcome to my&nbsp;
+                                    <span className="text-4xl lg:text-5xl text-emerald-500 hover:text-emerald-300 transition-colors duration-500">
+                                        Blog
+                                    </span>
+                                </p>
+                            )}
+                        </section>
+                        <section className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8 max-w-[95%] ">
+                            <PostsCards posts={posts} />
+                        </section>
+                    </main>
+                </>
+            )}
+            <Footer />
+        </div>
+    );
 }
--- frontend/src/pages/PostPage.jsx
+++ frontend/src/pages/PostPage.jsx
@@ -10,108 +10,108 @@ import Comments from "../components/Comments";
 import DOMPurify from "dompurify";
 
 export default function PostPage() {
-  const { user } = useContext(AuthContext);
-  const { id } = useParams();
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState("");
-  const [post, setPost] = useState([]);
-  const [comments, setComments] = useState([]);
+    const { user, loading: authLoading } = useContext(AuthContext);
+    const { id } = useParams();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
+    const [post, setPost] = useState([]);
+    const [comments, setComments] = useState([]);
 
-  useEffect(() => {
-    async function fetchPost() {
-      setLoading(true);
-      try {
-        const post = await fetch(
-          `${import.meta.env.VITE_BACKEND_URL}/posts/${id}`
-        );
-        if (post.status === 404) {
-          throw new Error("Post not Found");
+    useEffect(() => {
+        async function fetchPost() {
+            setLoading(true);
+            try {
+                const post = await fetch(
+                    `${import.meta.env.VITE_BACKEND_URL}/posts/${id}`
+                );
+                if (post.status === 404) {
+                    throw new Error("Post not Found");
+                }
+                const postData = await post.json();
+                setPost(postData.post);
+
+                const postComments = await fetch(
+                    `${import.meta.env.VITE_BACKEND_URL}/posts/${id}/comments`
+                );
+                const postCommentsData = await postComments.json();
+                setComments(postCommentsData.comments);
+            } catch (err) {
+                console.error(err);
+                setLoading(false);
+                setError("Failed to load post");
+                setPost([]);
+                setComments([]);
+            } finally {
+                setLoading(false);
+            }
         }
-        const postData = await post.json();
-        setPost(postData.post);
+        fetchPost();
+    }, [id]);
 
-        const postComments = await fetch(
-          `${import.meta.env.VITE_BACKEND_URL}/posts/${id}/comments`
-        );
-        const postCommentsData = await postComments.json();
-        setComments(postCommentsData.comments);
-      } catch (err) {
-        console.error(err);
-        setLoading(false);
-        setError("Failed to load post");
-        setPost([]);
-        setComments([]);
-      } finally {
-        setLoading(false);
-      }
-    }
-    fetchPost();
-  }, [id]);
-
-  return (
-    <div className="flex flex-col min-h-screen bg-neutral-900 overflow-x-hidden">
-      <NavBar />
-      {loading && (
-        <main className="flex-1 flex flex-col items-center justify-center">
-          <Spinner size="xl" />
-        </main>
-      )}
-      {error && (
-        <main className="flex-1">
-          <ErrorAlert message={error} />
-        </main>
-      )}
-      {!loading && !error && (
-        <>
-          <main className="flex-1 py-12 px-4 sm:px-6 lg:px-8">
-            <article className="max-w-3xl mx-auto space-y-10">
-              <header className="space-y-4 border-b border-neutral-800 pb-10">
-                <h1 className="text-5xl text-center font-bold text-emerald-500 hover:text-emerald-400 transition-colors duration-500 wrap-break-word">
-                  {post.title}
-                </h1>
-                {post.author && (
-                  <div className="text-neutral-400 text-sm flex items-center justify-center gap-2">
-                    <span className="wrap-break-word">By {post.author}</span>
-                  </div>
-                )}
-              </header>
-              <section
-                className="prose prose-lg prose-invert max-w-none text-neutral-300 leading-relaxed border-neutral-800 pb-10 wrap-break-word"
-                dangerouslySetInnerHTML={{
-                  __html: DOMPurify.sanitize(post.text),
-                }}
-              />
-              <section>
-                <h3 className="text-2xl font-semibold text-white mb-8">
-                  <div className="flex justify-between">
-                    <div>
-                      Comments{" "}
-                      <span className="text-emerald-500">
-                        ({comments.length})
-                      </span>
-                    </div>
-                    {user && (
-                      <Link
-                        to={`/post/${post.id}/comments/new`}
-                        className="bg-emerald-500 hover:bg-emerald-400 rounded-full p-2 transition-all duration-200 text-xs active:scale-100 hover:scale-105"
-                      >
-                        Create Comment
-                      </Link>
-                    )}
-                  </div>
-                </h3>
-                <Comments
-                  comments={comments}
-                  postId={id}
-                  setComments={setComments}
-                  setError={setError}
-                />
-              </section>
-            </article>
-          </main>
-        </>
-      )}
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="flex flex-col min-h-screen bg-neutral-900 overflow-x-hidden">
+            <NavBar />
+            {loading || authLoading && (
+                <main className="flex-1 flex flex-col items-center justify-center">
+                    <Spinner size="xl" />
+                </main>
+            )}
+            {error && (
+                <main className="flex-1">
+                    <ErrorAlert message={error} />
+                </main>
+            )}
+            {!loading && !error && !authLoading && (
+                <>
+                    <main className="flex-1 py-12 px-4 sm:px-6 lg:px-8">
+                        <article className="max-w-3xl mx-auto space-y-10">
+                            <header className="space-y-4 border-b border-neutral-800 pb-10">
+                                <h1 className="text-5xl text-center font-bold text-emerald-500 hover:text-emerald-400 transition-colors duration-500 wrap-break-word">
+                                    {post.title}
+                                </h1>
+                                {post.author && (
+                                    <div className="text-neutral-400 text-sm flex items-center justify-center gap-2">
+                                        <span className="wrap-break-word">By {post.author}</span>
+                                    </div>
+                                )}
+                            </header>
+                            <section
+                                className="prose prose-lg prose-invert max-w-none text-neutral-300 leading-relaxed border-neutral-800 pb-10 wrap-break-word"
+                                dangerouslySetInnerHTML={{
+                                    __html: DOMPurify.sanitize(post.text),
+                                }}
+                            />
+                            <section>
+                                <h3 className="text-2xl font-semibold text-white mb-8">
+                                    <div className="flex justify-between">
+                                        <div>
+                                            Comments{" "}
+                                            <span className="text-emerald-500">
+                                                ({comments.length})
+                                            </span>
+                                        </div>
+                                        {user && (
+                                            <Link
+                                                to={`/post/${post.id}/comments/new`}
+                                                className="bg-emerald-500 hover:bg-emerald-400 rounded-full p-2 transition-all duration-200 text-xs active:scale-100 hover:scale-105"
+                                            >
+                                                Create Comment
+                                            </Link>
+                                        )}
+                                    </div>
+                                </h3>
+                                <Comments
+                                    comments={comments}
+                                    postId={id}
+                                    setComments={setComments}
+                                    setError={setError}
+                                />
+                            </section>
+                        </article>
+                    </main>
+                </>
+            )}
+            <Footer />
+        </div>
+    );
 }
